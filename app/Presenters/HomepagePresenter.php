2421e76951153161aa27b97b4de046a130104bf0
jirka <jorgetimes2@gmail.com>
2022-11-13 18:00:52 +0100

    add missing rules

--- app/Presenters/HomepagePresenter.php
+++ app/Presenters/HomepagePresenter.php
@@ -62,19 +62,42 @@ final class HomepagePresenter extends Nette\Application\UI\Presenter
         //we are brave (or silly?) and truncate special characters also,
         //so that our future selves can have hard times to restore the text
         //from piggy latin back to english, with the correct punctuation letters
+        //(in which case, however, the database is to the rescue)
         $cleanInput = Strings::webalize(Strings::lower(Strings::trim($origin)));
         $wordsToTranslate = explode('-', $cleanInput);
 
-        //First rule of Piggy Latin: we don't want to append "ay" to words
-        //that formally start with a vowel (like 'you', which phonetically starts
-        //with 'u' consonant in fact), and, on the other hand, we definitely
-        //want to append "ay" to words that formally look like starting with
-        //a consonant but phonetically start with a vowel, like 'x-ray'
-        $vowelPattern = '/^([aeiou]|xr|yt).*/';
+        //First rule of Piggy Latin: when starting with a vowel
+        //we append "ay", however the 'y' character is not included since it is
+        //phonetically equivalent to a consonant (like 'you') and, on the other hand,
+        //words starting with 'xr' or 'yt', although formally starting with a consonant,
+        //sound like starting with a vowel, ('x-ray' or 'ytterbium')
+        $vowelPattern = '/^([aeiou]|xr|yt)(.*)$/';
+
+        //some words, like 'question' or 'square' contain vowels, although phonetically
+        //these vowels are "silent", we want to move the consonants plus 'qu' to the end
+        //and then append 'ay'
+        $consonantQuPattern = '/^([^aeiou]?)(qu)(.*)$/';
+
+        //if a word begins with a consonant or multiple consonants (i.e. consonant clusters)
+        //we move the consonants (or their clusters) to the end of the word and append 'ay'
+        $consonantPattern = '/^([^aeiou]+)(.*)$/';
+
+        //If a word contains a "y" after a consonant cluster or as the second letter
+        //in a two letter word it makes a vowel sound ('rhythm', 'my')
+        $clusterYPattern = '/^([^aeiou]+)(y)(.*)$/';
 
         foreach ($wordsToTranslate as $key => $wordToTranslate) {
-            if (preg_match($vowelPattern, $wordToTranslate)) {
+            if (preg_match($vowelPattern, $wordToTranslate, $matches)) {
                 $wordsToTranslate[$key] .= 'ay';
+            }
+            if (preg_match($consonantPattern, $wordToTranslate, $matches)) {
+                $wordsToTranslate[$key] = $matches[2] . '-' . $matches[1] . 'ay';
+            }
+            if (preg_match($consonantQuPattern, $wordToTranslate, $matches)) {
+                $wordsToTranslate[$key] = $matches[3] . '-' . $matches[1] . $matches[2] . 'ay';
+            }
+            if (preg_match($clusterYPattern, $wordToTranslate, $matches)) {
+                $wordsToTranslate[$key] = $matches[2] . $matches[3] . '-' . $matches[1] . 'ay';
             }
         }
 
